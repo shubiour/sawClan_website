ca2479d632a07e4cd6f5a3d62f5e8094d5cbf8c0
shubiour <sv95.shuvo@gmail.com>
2020-01-27 10:04:19 +0600

    second commit

---
+++ main/header.php
@@ -0,0 +1,47 @@
+<?php 
+//require 'config/config.php' ;
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Sabertooth Wolves</title>
+	
+	<!-- javaScripts -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<script src="assets\js\createJoinClass.js"></script>
+		<script src="assets/js/demo.js"></script>
+		<script src="assets/js/jquery.jcrop.js"></script>
+	    <script src="assets/js/jcrop_bits.js"></script>
+
+	<!-- css -->
+	  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+	  <link rel="stylesheet" type="text/css" href="assets\css\styling.css">
+	  <link rel="shortcut icon" type="image/png" href="assets/images/Sa.W_Round_Corner_For_Discord_DP.jpg">
+	  <link rel="stylesheet" href="assets/css/jquery.Jcrop.css" >
+</head>
+
+<body>
+  
+  <div class="background"></div>
+	<div class="top_bar">
+		<div class="logo">
+		<a href="index.php"><img src="assets/images/IMG-20190912-WA0000.png" alt="index.php"></a>
+		</div>
+		<nav>
+			<a href="">Teams</a> </li>
+			<a href="">Events</a>
+			<a href="">Contents</a>
+			<a href="">Contact</a>
+			<a href="">About</a>
+		</nav>	
+	</div>
+			
+</div>
+	
+
+	
---
+++ main/index.php
@@ -0,0 +1,172 @@
+<?php 
+include("header.php");
+?>
+<div class="background">
+    <div class="main_column">
+        <img src="assets/images/SaW_Profile_Picture_Dark__500px_2x11111_.png" >
+        <h3>Powered by</h3>
+    </div>
+
+    <div class="main_column2">
+        <img src="assets/images/GIGABYTE.png" >
+    </div>
+
+    <div class="main_column3">
+        <img src="assets/images/aoruslogo.png" >
+    </div>
+
+    <div class="main_column4">
+        <img src="assets/images/clubg1IT.png" >
+    </div>
+    <div class="teams">
+    <p>Our Teams</p>
+    <div class="gamesContainer">
+            <div class="box">
+             <a href="">   
+                <img src="assets/images/Rainbow6.jpg" alt="">
+            </a>
+             </div>
+            <div class="box">
+            <a href="">
+                <img src="assets/images/CSGO_01a70e93-e10c-4bd2-a53f-8a4a554ecbc9.webp" alt="">
+            </a>    
+            </div>
+            <div class="box">
+             <a href="">
+                 <img src="assets/images/PUBG_copy.webp" alt="">
+             </a>   
+            </div>
+            <div class="box">
+             <a href="">   
+            <img src="assets/images/maxresdefault.jpg" alt="">
+            </a>
+            </div>
+            <div class="box">
+            <a href="">
+            <img src="assets/images/FIFA_22e1c330-23da-450e-ba30-0cc14109eacd.webp" alt="">
+            </a>
+            </div>       
+            <div class="box">
+             <a href="">
+            <img src="assets/images/https___blogs-images.forbes.com_davidthier_files_2019_03_https___blogs-images.forbes.com_insertcoin_files_2019_03_apex-legendsasdasda1.jpg" alt="">
+            </a>
+            </div>                         
+        </div>
+        <div class="viewMore">
+            <a href=""><p>View more</p></a>
+        </div>
+        
+    </div>
+
+    <div class="schedule">
+        <p>Schedule</p>
+        <div class="upComingEventsContainer">
+                <div class="match">
+                     <div class="matchContents time"><div class="matchDates">Sep 26, 2019</div></div>
+                     <div class="matchContents verses">
+                             <div class="matchContents team1">
+                                    <div class="IGN">Sa.W</div>
+                                    <img class="teamLogo" src="assets/images/IMG-20190912-WA0000.png" alt="">
+                                    <div class="hastag">#</div>
+                             </div>
+                             <div class="t1Vst2">vs</div>
+                             <div class="matchContents team2">
+                                    <div class="hastag">#</div>
+                                    <img class="teamLogo" src="assets/images/tsm.png" alt="">
+                                    <div class="IGN">TMS</div>
+                                </div> 
+                     </div>
+                     <div class="matchContents">
+                           <div class="gameLogoContainer r6">
+                                     <img class="gameLogo R6" src="assets/images/rainbow-six-siege-logo-png-3-300x94.png" alt=""> 
+                            </div>    
+                     </div>
+                </div>
+                <div class="match">
+                <div class="matchContents time"><div class="matchDates">Sep 28, 2019</div></div>
+                     <div class="matchContents verses">
+                             <div class="matchContents team1">
+                                    <div class="IGN">Sa.W</div>
+                                    <img class="teamLogo" src="assets/images/IMG-20190912-WA0000.png" alt="">
+                                    <div class="hastag">#</div>
+                             </div>
+                             <div class="t1Vst2">vs</div>
+                             <div class="matchContents team2">
+                                    <div class="hastag">#</div>
+                                    <img class="teamLogo" src="assets/images/Untitled-1.png" alt="">
+                                    <div class="IGN">DF</div>
+                                </div> 
+                     </div>
+                     <div class="matchContents">
+                           <div class="gameLogoContainer pubg">
+                                     <img class="gameLogo" src="assets/images/d50c68fcb14bffb510d162c0bd656f3f.png" alt=""> 
+                            </div>    
+                     </div>
+                </div>
+                <div class="match">
+                <div class="matchContents time"><div class="matchDates">Sep 30, 2019</div></div>
+                     <div class="matchContents verses">
+                             <div class="matchContents team1">
+                                    <div class="IGN">Sa.W</div>
+                                    <img class="teamLogo" src="assets/images/IMG-20190912-WA0000.png" alt="">
+                                    <div class="hastag">#</div>
+                             </div>
+                             <div class="t1Vst2">vs</div>
+                             <div class="matchContents team2">
+                                    <div class="hastag">#</div>
+                                    <img class="teamLogo" src="assets/images/c9.png" alt="">
+                                    <div class="IGN">C9</div>
+                                </div> 
+                     </div>
+                     <div class="matchContents">
+                           <div class="gameLogoContainer r6">
+                                     <img class="gameLogo R6" src="assets/images/rainbow-six-siege-logo-png-3-300x94.png" alt=""> 
+                            </div>    
+                     </div>
+                </div>
+                <div class="match">
+                <div class="matchContents time"><div class="matchDates">Oct 1, 2019</div></div>
+                     <div class="matchContents verses">
+                             <div class="matchContents team1">
+                                    <div class="IGN">Sa.W</div>
+                                    <img class="teamLogo" src="assets/images/IMG-20190912-WA0000.png" alt="">
+                                    <div class="hastag">#</div>
+                             </div>
+                             <div class="t1Vst2">vs</div>
+                             <div class="matchContents team2">
+                                    <div class="hastag">#</div>
+                                    <img class="teamLogo" src="assets/images/fazeclan-002.jpg" alt="">
+                                    <div class="IGN">FZE</div>
+                                </div> 
+                     </div>
+                     <div class="matchContents">
+                           <div class="gameLogoContainer csgo">
+                                     <img class="gameLogo" src="assets/images/csgo.png" alt=""> 
+                            </div>    
+                     </div>
+                </div>
+        </div>   
+    </div>
+     
+
+    <div class="footer">
+        <img src="assets/images/Layer 0.png" alt="">
+        <div class="socialLinkContainer">
+              <div class="sbox">
+                  <a href="https://www.facebook.com/SabertoothWolvesOfficial"><i class="fab fa-facebook-square links"></i></a>
+                  <a href="https://www.youtube.com/channel/UC6q_n0JZpO1cr54osE_uf7Q"><i class="fab fa-youtube"></i></a>
+              </div>
+              <div class="sbox">
+                  <a href=""><i class="fab fa-twitch"></i></a>
+                  <a href=""><i class="fab fa-twitter"></i></a>
+              </div>
+        </div>
+        <div class="footerTitle">Sabertooth Wolves</div> 
+        <div class="footerCopyright">Copyright © 2019 SV95. All Rights Reserved.</div>   
+      
+
+</div>     
+
+
+</body>
+</html>
